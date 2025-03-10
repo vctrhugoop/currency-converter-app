7e3cc68758209d056d71c13ab2ffc140c0988a81
vctrhugoop <vhdestroy@hotmail.com>
2025-03-10 17:01:08 -0300

    ✨ feat: Implement currency conversion functionality in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,36 @@
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
+const footer = document.querySelector('main footer');
 
 amount.addEventListener('input', () => {
   const hasCharactersRegex = /\D+/g
 
   amount.valeu = amount.value.replace(hasCharactersRegex, '');
 })
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value) {
+    case 'USD':
+      convertCurrency(amount.value, 5.3, 'US$')
+      break
+    case 'EUR':
+      convertCurrency(amount.value, 6.2, '€')
+      break
+    case 'GBP':
+      convertCurrency(amount.value, 7.1, '£')
+      break
+  }
+}
+
+function convertCurrency(amount, price, symbol,) {
+  try {
+    footer.classList.add('show-result')
+  } catch (error) {
+    console.error(error)
+    footer.classList.remove('show-result')
+    alert("Não foi possível realizar a conversão")
+  }
+}
